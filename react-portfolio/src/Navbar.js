45dadd871d9c21496ebd9a0750ba49d02cb8ee93
Sarness Prasad <sunny776@yahoo.com>
2021-05-25 16:20:43 -0700

    finishing the search button

---
+++ react-portfolio/src/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <nav className="navbar">  
+      <h1>React Portfolio</h1>
+      <div className= "links">
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Github</a>
+      </div>
+    
+    
+    
+    
+    </nav>
+  
+  
+    )
+}
+
+export default Navbar;
